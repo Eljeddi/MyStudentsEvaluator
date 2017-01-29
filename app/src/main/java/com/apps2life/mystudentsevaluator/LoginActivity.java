d29ac80c35f346763aca79a701b938adb1128e89
Eljeddi Oussema <oussemaeljeddi@gmail.com>
2017-01-29 22:02:22 +0100

    now we are done with the login , not that cool , but coool :)

--- app/src/main/java/com/apps2life/mystudentsevaluator/LoginActivity.java
+++ app/src/main/java/com/apps2life/mystudentsevaluator/LoginActivity.java
@@ -1,39 +1,82 @@
 package com.apps2life.mystudentsevaluator;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+//android.graphics.drawable.RippleDrawable
+import android.graphics.drawable.RippleDrawable;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-
+import com.facebook.AccessToken;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static android.R.attr.data;
+import static android.R.attr.dialogMessage;
 import static android.R.attr.password;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "Login";
+    private CallbackManager mCallbackManager;
+    private EditText emailf;
+    private EditText pwd;
+    public ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        emailf=(EditText)findViewById(R.id.emailid);
+        pwd=(EditText)findViewById(R.id.passwordid);
+        final Intent intent=new Intent(LoginActivity.this,MainActivity.class);
         AppEventsLogger.activateApp(this);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.apps2life.mystudentsevaluator",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
 
-        // initialize the FirebaseAuth instance
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
         mAuth = FirebaseAuth.getInstance();
-        //initializethe AuthStateListener method so you can track whenever the user signs in or out.
+
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -48,25 +91,89 @@ public class LoginActivity extends AppCompatActivity {
                 // ...
             }
         };
+
+
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.button_facebook_login);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+                startActivity(intent);
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+                // ...
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+                // ...
+            }
+
+        });
+
+        // ...
     }
+
+
     @Override
     public void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
     }
+
     @Override
     public void onStop() {
         super.onStop();
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
-    }
-    public void createAccount (View view) {
-        //get the email and password value frm the xml file
-        String email = ((EditText) findViewById(R.id.emailid)).getText().toString().trim();
-        String password = ((EditText) findViewById(R.id.passwordid)).getText().toString().trim();
 
-        //method that takes login nd pwd and validate them or not
+    }
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+    //log in old users
+    public void fLogIn(View view){
+
+        signIn(emailf.getText().toString(),pwd.getText().toString());
+    }
+    //sign in new users
+    public void newUserAccountCreation(View view){
+        createAccount(emailf.getText().toString(),pwd.getText().toString());
+    }
+    /////code for creating new account
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        showProgressDialog();
+
+        // [START create_user_with_email]
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -79,11 +186,117 @@ public class LoginActivity extends AppCompatActivity {
                         if (!task.isSuccessful()) {
                             Toast.makeText(LoginActivity.this, R.string.auth_failed,
                                     Toast.LENGTH_SHORT).show();
+                        }else {
+                            final Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
                         }
 
-                        // ...
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    //sign in users
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        if(task.isSuccessful()){
+                            final Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(intent);
+                        }
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = emailf.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            //email.setError();
+            valid = false;
+        } else {
+            //email.setError(null);
+        }
+
+        String password = pwd.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            pwd.setError("Required.");
+            valid = false;
+        } else {
+            pwd.setError(null);
+        }
+
+        return valid;
+    }
+
+// ...
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Pass the activity result back to the Facebook SDK
+            mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+        }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        // [START_EXCLUDE silent]
+        //showProgressDialog
+        // [END_EXCLUDE]
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
                     }
                 });
     }
+    // [END auth_with_facebook]
 
 }
